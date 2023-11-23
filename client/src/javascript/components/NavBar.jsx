539df1191c81d85c47d405a8300a885f8ca64043
mahanshAdtiya <mahansh21334@iiitd.ac.in>
2023-11-23 13:02:13 +0530

    updated navbar-cursor-pointer not working

--- client/src/javascript/components/NavBar.jsx
+++ client/src/javascript/components/NavBar.jsx
@@ -1,32 +1,12 @@
-import React, {useState, useEffect } from "react";
+import React from "react";
 import logo from "../../assets/logo.png"
+import avatar from "../../assets/account_avatar.png"
 
-const TOP_OFFSET = 66;
 const NavBar = ()=>{
-    const[showBackground, setShowBackground] = useState(false);
-
-    useEffect(() =>{
-        const handleScroll = () =>{
-            if(window.scrollY >= TOP_OFFSET){
-                setShowBackground(true);
-            }
-            else{
-                setShowBackground(false);
-            }
-        }
-
-        window.addEventListener('scroll', handleScroll);
-
-        return () =>{
-            window.removeEventListener('scroll', handleScroll);
-        }
-    },[]);
-
     return(
-        <div className="w-full fixed z-40">
+        <div className="w-full">
             <div
-            className=
-            {`px-4
+            className="px-4
             md:px-16
             py-6
             flex
@@ -34,9 +14,13 @@ const NavBar = ()=>{
             items-center
             transition
             duration-1000
-            ${showBackground ? 'bg-zinc-900 bg-opacity-90' : ''}
-            `}>
-                <img className="h-4 lg:h-7 "  src={logo} alt='Logo'/>
+            bg-zinc-900 
+            bg-opacity-90
+            cursor-pointer"
+            >
+                <div className="cursor-pointer">
+                    <img className="h-14 lg:h-17"  src={logo} alt='Logo'/>
+                </div>
 
                 <div className="
                 flex-row
@@ -53,7 +37,7 @@ const NavBar = ()=>{
                     <div className="flex flex-row items-center gap-2 cursor-pointer relative">
 
                         <div className="w-10 h-10 lg:w-16 lg:h-16 overflow-hidden">
-                            <img src='/images/account_avatar.png' alt='Profile'/>
+                            <img src={avatar} alt='Profile'/>
                         </div>
 
                     </div>
